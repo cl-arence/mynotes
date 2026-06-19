9f849b0610c70deb02fb78371fab636108fe9a39
cl-arence <wasilwa91@gmail.com>
2026-06-18 20:55:44 -0700

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 
 
@@ -36,13 +37,17 @@ class Homepage extends StatelessWidget {
           
           switch (asyncSnapshot.connectionState) {
             case ConnectionState.done:
-          //   final user = FirebaseAuth.instance.currentUser;
-          //   if (user?.emailVerified ?? false) {
-          //  return const Text('Done');
-          //   } else {
-          //     return const VerifyEmailView();
-          //   }
-          return const LoginView();
+             final user = FirebaseAuth.instance.currentUser;
+             if (user != null) {
+              if (user.emailVerified) {
+                Text('Email is verified');
+             } else {
+              return const VerifyEmailView();
+             }
+             } else {
+              return const LoginView();
+             }
+             return const Text('Done');
             default:
               return const CircularProgressIndicator();
           }
