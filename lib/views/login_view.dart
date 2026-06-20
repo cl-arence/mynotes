5fcc05b6bbf087d3b3dce037bd631b233012a706
cl-arence <wasilwa91@gmail.com>
2026-06-19 20:25:18 -0700

    step-5

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
+import 'package:mynotes/constants/routes.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
 
@@ -62,11 +64,11 @@ class _LoginViewState extends State<LoginView> {
                   final email = _email.text;
                   final password = _password.text;
                   try {
-                    final userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                     await FirebaseAuth.instance.signInWithEmailAndPassword(
                       email: email,
                       password: password,
                     );
-                    Navigator.of(context).pushNamedAndRemoveUntil('/notes/', (route) => false,);
+                    Navigator.of(context).pushNamedAndRemoveUntil(notesRoute, (route) => false,);
                   } on FirebaseAuthException catch (e) {
                     if (e.code == 'user-not-found') {
                       devtools.log('User not found');
@@ -78,7 +80,7 @@ class _LoginViewState extends State<LoginView> {
                   child: const Text('Login')
                 ),
             TextButton(onPressed: () {
-                Navigator.of(context).pushNamedAndRemoveUntil('/register/', (route) => false);
+                Navigator.of(context).pushNamedAndRemoveUntil(registerRoute, (route) => false);
             },
               child: const Text('Not registered yet? Register here!'),
             )
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
-
+import 'package:mynotes/constants/routes.dart';
 
 class RegisterView extends StatefulWidget {
   
@@ -85,7 +85,7 @@ class _RegisterViewState extends State<RegisterView> {
                   child: const Text('Register'),
                 ),
                 TextButton(
-                  onPressed: () {Navigator.of(context).pushNamedAndRemoveUntil('/login/', (route) => false);},
+                  onPressed: () {Navigator.of(context).pushNamedAndRemoveUntil(loginRoute, (route) => false);},
                   child: const Text('Already registered? Login here!'),
                 ),
               ],
